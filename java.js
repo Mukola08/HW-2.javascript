3b0cd7262cb4703f02d25e54a1f2427b03ea34b2
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-02-07 21:30:31 +0200

    push

---
+++ java.js
@@ -0,0 +1,32 @@
+// Задача 1
+const Celsius = 100;
+const Fahrenheit = (100 - 273.15) * 1.8 + 32;
+console.log(Fahrenheit);
+
+// Задача 2
+const day = 31;
+const Hour = day * 24;
+const Minute = Hour * 60;
+console.log(Hour);
+console.log(Minute);
+
+// Задача 3
+let Health = 100;
+let Energy = 100;
+Health -= 10;
+Energy -= 25;
+console.log(Health);
+console.log(Energy);
+
+
+
+
+// Задача 4
+const Sum = 200;
+const Discount = Sum * 0.10;
+const Action = Sum - Discount;
+console.log(Action);
+
+
+
+
